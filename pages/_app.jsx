236e113e0735209c218b8c37fd3f22e7a957a08e
Ivanrizkys <ivnrzkys@gmail.com>
2022-01-26 14:20:59 +0700

    Initialize theme provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+			<CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
