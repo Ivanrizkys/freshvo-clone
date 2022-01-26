6f3a6fd0bf13e82b83c9f49268de21b6c7aada78
Ivanrizkys <ivnrzkys@gmail.com>
2022-01-26 14:17:07 +0700

    Add: overflow auto

--- components/atoms/Layout/index.jsx
+++ components/atoms/Layout/index.jsx
@@ -6,9 +6,9 @@ const Layout = ({ children }) => {
             <Head>
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
                 <title>Freshvo</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/warung-customer.appspot.com/o/freshvo-favicon-300x300.png?alt=media&token=059cc4ba-a08e-47aa-b3f2-e7686126343b" />
             </Head>
-            <div>{children}</div>
+            <div style={{overflowY: "auto"}}>{children}</div>
         </>
     );
 };
