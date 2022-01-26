4f2d5b637aa4fdc27b404bbaccd8794f3cea0175
Ivanrizkys <ivnrzkys@gmail.com>
2022-01-26 14:23:59 +0700

    Cretate: theme for MUI configuration

---
+++ theme/index.js
@@ -0,0 +1,27 @@
+// import { createTheme } from "@mui/system";
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        background: {
+            primary: "#FAFAFA",
+            secondary: "rgb(255, 255, 255)",
+            white: "#FFFF"
+        },
+        text: {
+            primary: "rgba(0, 0, 0, 0.87)",
+            secondary: "#4E5356",
+            gray: "#808080",
+            green: "#00A739"
+        },
+        buttonPrimay: {
+            main: "rgb(0, 167, 57)",
+            contrastText: "#FFFF"
+        }
+    },
+    typography: {
+        fontFamily: "Montserrat",
+    },
+})
+
+export default theme;
