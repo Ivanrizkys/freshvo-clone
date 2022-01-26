26e879a5df4777b7e33cb16dd1b9b14a38bc18e1
Ivanrizkys <ivnrzkys@gmail.com>
2022-01-26 14:21:27 +0700

    Add: new font family

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,14 @@ export default function Document() {
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/icon?family=Material+Icons"
                 />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                ></link>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans:wght@300;400;600;700&display=swap"
+                    rel="stylesheet"
+                ></link>
             </Head>
             <body>
                 <Main />
