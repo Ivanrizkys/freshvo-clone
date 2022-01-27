e48b0ed42fd43d6e3fe70288f2f5dee2ae4ab8a6
Ivanrizkys <ivnrzkys@gmail.com>
2022-01-27 13:35:40 +0700

    adding border radius in banner

--- components/organism/NewInformation/index.jsx
+++ components/organism/NewInformation/index.jsx
@@ -7,16 +7,19 @@ import SwiperCore, { Pagination, Autoplay } from 'swiper';
 
 SwiperCore.use([Pagination, Autoplay]);
 
+
+// autoplay={{ "delay": 2500, "disableOnInteraction": false }}
+
 const NewInformation = ({banners}) => {    
     return (
         <ContainerMini sx={{mt: "28px", mb: "15px"}}>
             <Typography sx={{fontSize: "13px", fontWeight: "700"}}>Informasi Terbaru</Typography>
-            <Typography sx={{fontSize: "10px", fontWeight: "400", color: "rgb(82, 87, 92)", mb: "12px"}}>Informasi terbaru hanya untukmu</Typography>
-            <Swiper pagination={{type: 'bullets', el: '.swiper-pagination-custom'}} slidesPerView={1.15} spaceBetween={20} autoplay={{ "delay": 2500, "disableOnInteraction": false }}>
+            <Typography sx={{fontSize: "10px", fontWeight: "400", color: "rgb(82, 87, 92)", mb: "12px"}}>Informasi terbaru hanya untukmu!</Typography>
+            <Swiper pagination={{type: 'bullets', el: '.swiper-pagination-custom'}} slidesPerView={1.15} spaceBetween={20}>
                 {banners.map(data => {
                     return (
                         <SwiperSlide key={data.id}>
-                            <Box sx={{cursor: "pointer"}}>
+                            <Box sx={{width: "360px", height: "150px",cursor: "pointer", borderRadius: "7px 0 0 7px", overflow: "hidden"}}>
                                 <Image src={data.image.url} width={360} height={150} alt="banner" />
                             </Box>
                         </SwiperSlide>
