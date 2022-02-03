9052e0dd1a845efe96aca4377004b12684a4059a
Ivanrizkys <ivnrzkys@gmail.com>
2022-02-03 16:35:12 +0700

    adding function to convert value in rupiah and function to calculate discount

--- utils/convertion.js
+++ utils/convertion.js
@@ -1,9 +1,11 @@
+// * get discount
 const getDiscount = (normalPrice, discountPrice) => {
     const difference = normalPrice - discountPrice
     const result = difference / normalPrice * 100
     return Math.round(result)
 }
 
+// * get format in rupiah
 const formatRupiah = (value) => {
     const reverse = value.toString().split('').reverse().join('')
     const split = reverse.match(/\d{1,3}/g)
