5b4eac1c85d0b043859dca0d880d11f161c9f081
Ivanrizkys <ivnrzkys@gmail.com>
2022-02-03 16:36:23 +0700

    Adding cart feature

--- components/organism/Product/index.jsx
+++ components/organism/Product/index.jsx
@@ -1,18 +1,109 @@
 import "swiper/css";
 import Image from "next/image";
-import { useEffect } from "react";
+import { getIndex } from "@/utils/helper"
+import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useState, useRef } from "react";
 import { Box, Grid, Typography } from "@mui/material";
-import SwiperCore, { Pagination, Autoplay } from 'swiper';
-import ButtonPrimary from "@/components/atoms/ButtonPrimary";
+import ButtonMini from "@/components/atoms/ButtonMini";
 import ContainerMini from "@/components/atoms/ContainerMini";
+import ButtonPrimary from "@/components/atoms/ButtonPrimary";
 import { getDiscount, formatRupiah } from "@/utils/convertion";
 
+SwiperCore.use([Pagination]);
 
 const Product = ({product}) => {
+    const [cart, setCart] = useState([])
+    const [cartTotal, setCartTotal] = useState({
+        totalItem: 0,
+        totalPrice: 0,
+    })
+    const didMountRef = useRef(false)
+
     useEffect(() => {
-        SwiperCore.use([Pagination]);
-    }, [])
+        switch(didMountRef.current) {
+            case true:
+                const items = JSON.stringify(cart)
+                localStorage.setItem('items', items)
+                const totalItems = JSON.stringify(cartTotal)
+                localStorage.setItem('totalItems', totalItems)
+                return
+            default:
+                // * only run when initial render
+                // * get data in localstorege if exist and set to state
+                const getItem = JSON.parse(localStorage.getItem('items'))
+                const getTotalItem = JSON.parse(localStorage.getItem('totalItems'))
+                if (getItem && getTotalItem) {
+                    setCart(getItem)
+                    setCartTotal(getTotalItem)
+                }
+        }
+        didMountRef.current = true
+    },[cart, cartTotal])
+
+    // * handle the addition of the first item
+    const addItemHandler = (id, name, price, maximumOrder, e) => {
+        e.preventDefault();
+        setCart([
+            ...cart,
+            {
+                id_product: id,
+                product_name: name,
+                product_price: price,
+                total_price: price,
+                totalItem: 1,
+                inOrder: true,
+                maximumOrder
+            }
+        ])
+        setCartTotal({
+            totalItem: cartTotal.totalItem + 1,
+            totalPrice: cartTotal.totalPrice + price
+        })
+    }
+
+    // * handle remove or reduction items
+    const removeItemHandler = (id, price, e) => {
+        e.preventDefault();
+
+        const index = getIndex(cart, id)
+        const currentValue = cart[index].totalItem
+        if (currentValue === 1) {
+            cart.splice(index, 1)
+            setCart([...cart])
+            setCartTotal({
+                totalItem: cart.length,
+                totalPrice: cartTotal.totalPrice - price
+            })
+            return
+        }
+        cart[index].total_price -= price
+        cart[index].totalItem -= 1
+        setCart([...cart])
+        setCartTotal({
+            ...cartTotal,
+            totalPrice: cartTotal.totalPrice - price
+        })
+    }
+
+    // * handle addition item
+    const addItemsHandler = (id, price, e) => {
+        e.preventDefault()
+
+        const index = getIndex(cart, id)
+        const getMaimumOrder = cart[index].maximumOrder
+        if (getMaimumOrder === cart[index].totalItem) {
+            return
+        }
+        // * update data in state
+        cart[index].total_price += price
+        cart[index].totalItem += 1
+        setCart([...cart])
+        setCartTotal({
+            ...cartTotal,
+            totalPrice: cartTotal.totalPrice + price
+        })
+    }
     
     return (
         <>
@@ -48,8 +139,20 @@ const Product = ({product}) => {
                                         <Typography sx={{fontSize: "10px"}}>/kg</Typography>
                                     </Box>
                                 </Box>
-                                <Box sx={{display: "flex", justifyContent: "center"}}>
-                                    <ButtonPrimary>Beli</ButtonPrimary>
+                                <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                    {cart[getIndex(cart, data.id)] ?
+                                        <>
+                                            <ButtonMini variant="buttonTransparant" onClick={removeItemHandler.bind(this, data.id, data.price)}>-</ButtonMini>
+                                            <Typography sx={{width: "58px", textAlign: "center", fontSize: "13px", fontWeight: "800"}}>{cart[getIndex(cart, data.id)].totalItem}</Typography>
+                                            {cart[getIndex(cart, data.id)].totalItem === data.maximumOrderQuantity ? 
+                                                <ButtonMini variant="buttonDisable" onClick={addItemsHandler.bind(this, data.id, data.price)}>+</ButtonMini>
+                                                :
+                                                <ButtonMini variant="buttonPrimay" onClick={addItemsHandler.bind(this, data.id, data.price)}>+</ButtonMini>
+                                            }
+                                        </>
+                                        :
+                                        <ButtonPrimary onClick={addItemHandler.bind(this, data.id, data.name, data.price, data.maximumOrderQuantity)}>Beli</ButtonPrimary>
+                                    }
                                 </Box>
                                 {data.isPromo &&
                                     <Box sx={{position: "absolute", top: "0", left: "0", backgroundColor: "#00A739", opacity: "0.8"}}>
@@ -61,8 +164,29 @@ const Product = ({product}) => {
                     )
                 })}
             </Swiper>
-            {/* <Box sx={{width: "92%", maxWidth: "442px", height: "75px", backgroundColor: "rgb(0, 167, 57)", position: "fixed", bottom: "70px", mb: "auto"}}></Box>     */}
-            <Box sx={{width: "150px", height: "60px",mt: "5px", visibility: "visible"}}></Box>
+            {cartTotal.totalItem > 0 &&
+                <Box sx={{width: "92%", maxWidth: "442px", position: "fixed", bottom: "70px", zIndex: "100"}}>
+                    <ContainerMini sx={{backgroundColor: "rgb(0, 167, 57)", padding: "8px 16px", borderRadius: "5px"}}>
+                        <Grid container sx={{alignItems: "center"}}>
+                            <Grid item xs={2}>
+                                <Image src="/cart-icon.svg" height={24} width={24} alt="cart-icon" />
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Typography sx={{fontSize: "14px", fontWeight: "bold", color: "#FFFF"}}>{cartTotal.totalItem} Item</Typography>
+                                <Typography sx={{fontSize: "12px", color: "#FFFF", width: "103px"}}>Freshvo Cilandak (Jabodetabek)</Typography>
+                            </Grid>
+                            <Grid item xs={2} sx={{display: "flex", alignItems: "center"}}>
+                                <Typography sx={{fontSize: "14px", fontWeight: "bold", color: "#FFFF", textAlign: "center"}}>{formatRupiah(cartTotal.totalPrice)}</Typography>
+                            </Grid>
+                        </Grid>
+                    </ContainerMini>    
+                </Box>
+            }
+            {cartTotal.totalItem > 0 ?
+                <Box sx={{width: "150px", height: "120px",mt: "5px", visibility: "visible"}}></Box>
+                :
+                <Box sx={{width: "150px", height: "60px",mt: "5px", visibility: "visible"}}></Box>
+            }
         </>
     )
 }
