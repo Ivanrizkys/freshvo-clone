cd7846eb256d5a07f056141360d2047021d61e04
Ivanrizkys <ivnrzkys@gmail.com>
2022-02-03 16:37:48 +0700

    Adding new props

--- components/atoms/ButtonPrimary/index.jsx
+++ components/atoms/ButtonPrimary/index.jsx
@@ -1,8 +1,8 @@
 import Button from '@mui/material/Button';
 
-const ButtonPrimary = ({children}) => {
+const ButtonPrimary = ({children, ...nativeProps}) => {
     return (
-        <Button color='buttonPrimay' variant="contained" sx={{backgroundColor: "rgb(0, 167, 57)", fontSize: "13px", py: "2px", px: "50px"}}>{children}</Button>
+        <Button color='buttonPrimay' variant="contained" sx={{backgroundColor: "rgb(0, 167, 57)", fontSize: "13px", py: "2px", px: "50px"}} {...nativeProps}>{children}</Button>
     )
 }
 
