d36ed14040bf20f58390f7848d1cc02b74975272
Ivanrizkys <ivnrzkys@gmail.com>
2022-02-04 15:55:14 +0700

    Adding new props

--- components/atoms/Input/index.jsx
+++ components/atoms/Input/index.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
 
-const Input = () => {
+const Input = ({reff, ...nativeProps}) => {
     return (
         <form style={{display: "flex", width: "100%", justifyContent: "center"}}>
-            <div style={{display: "flex", justifyContent: "center", width: "15%", marginTop: "16px", backgroundColor: "#F1F2F6", borderRadius: "100px 0 0 100px"}}>
+            <div {...nativeProps} style={{display: "flex", justifyContent: "center", width: "15%", marginTop: "16px", backgroundColor: "#F1F2F6", borderRadius: "100px 0 0 100px", cursor: "pointer"}}>
                 <Image src="/search-icon.svg" width={24} height={24} alt="search-icon"/>
             </div>
-            <input type="text" placeholder="Cari produk" style={{width: "85%", height: "50px", borderRadius: "0 100px 100px 0", marginTop: "16px", backgroundColor: "#F1F2F6", outline: "none", border: "none"}} />
+            <input ref={reff} type="text" placeholder="Cari produk" style={{width: "85%", height: "50px", borderRadius: "0 100px 100px 0", marginTop: "16px", backgroundColor: "#F1F2F6", outline: "none", border: "none", color: "", fontSize: "13px", fontWeight: "500"}} />
         </form>
     )
 }
