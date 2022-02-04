9ad8cfa4bf4c862794d25e6176a77dbebde92c24
Ivanrizkys <ivnrzkys@gmail.com>
2022-02-04 15:56:43 +0700

    Fixing pagination bug and adding new responsive breakpoint in banner image

--- components/organism/NewInformation/index.jsx
+++ components/organism/NewInformation/index.jsx
@@ -1,21 +1,20 @@
 import "swiper/css";
+import "swiper/css/pagination";
 import Image from "next/image";
 import { Box, Typography } from "@mui/material"
 import { Swiper, SwiperSlide } from "swiper/react";
 import ContainerMini from "@/components/atoms/ContainerMini";
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
+import { PaginationCustom } from './styles'
 
 SwiperCore.use([Pagination, Autoplay]);
-
-
-// autoplay={{ "delay": 2500, "disableOnInteraction": false }}
-
+// slidesPerView={1.15} spaceBetween={30}
 const NewInformation = ({banners}) => {    
     return (
         <ContainerMini sx={{mt: "28px", mb: "15px", position: "relative"}}>
             <Typography sx={{fontSize: "13px", fontWeight: "700"}}>Informasi Terbaru</Typography>
             <Typography sx={{fontSize: "10px", fontWeight: "400", color: "rgb(82, 87, 92)", mb: "12px"}}>Informasi terbaru hanya untukmu!</Typography>
-            <Swiper pagination={{type: 'bullets', el: '.swiper-pagination-custom'}} slidesPerView={1.15} spaceBetween={20}>
+            <Swiper pagination={{el: '.swiper-pagination',type: 'bullets'}} autoplay={{ "delay": 2500, "disableOnInteraction": false }} breakpoints={{100: {slidesPerView: 1}, 420: {slidesPerView: 1.15, spaceBetween: 30}}}>
                 {banners.map(data => {
                     return (
                         <SwiperSlide key={data.id}>
@@ -26,9 +25,10 @@ const NewInformation = ({banners}) => {
                     )
                 })}
             </Swiper>
-            <Box className="swiper-pagination-custom" sx={{mt: "10px"}}></Box>
-            {/* <div className="swiper-pagination-custom"></div> */}
-            {/* <p>Hallo sayang</p> */}
+            <Box sx={{mt: "10px"}}></Box>
+            <PaginationCustom>
+                <div className="swiper-pagination"></div>
+            </PaginationCustom>
         </ContainerMini>
     )
 }
